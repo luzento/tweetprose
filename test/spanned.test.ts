aa7bb18d7c1d6bc67dbe53bf5dcfa735fbc1e0cb
Joe Flateau <joe@joeflateau.net>
2022-01-24 11:48:06 -0500

    more testing

--- test/spanned.test.ts
+++ test/spanned.test.ts
@@ -1,7 +1,7 @@
 import { Spanned } from '../src/Spanned';
 
 describe('spanned', () => {
-  it('should find link', () => {
+  it('should find https:// link', () => {
     expect(
       Spanned.parse(`foo https://example.org bar`, {
         maxLength: 280,
@@ -9,7 +9,7 @@ describe('spanned', () => {
       })
     ).toHaveLength(4 + 23 + 4);
   });
-  it('should find link', () => {
+  it('should find domain only link', () => {
     expect(
       Spanned.parse(`foo example.org bar`, {
         maxLength: 280,
@@ -17,9 +17,17 @@ describe('spanned', () => {
       })
     ).toHaveLength(4 + 23 + 4);
   });
-  it('should find link', () => {
+  it('should find subdomain links', () => {
     expect(
-      Spanned.parse(`foo example.co.uk bar`, {
+      Spanned.parse(`foo www2.example.co.uk bar`, {
+        maxLength: 280,
+        countUrlsAs: 23,
+      })
+    ).toHaveLength(4 + 23 + 4);
+  });
+  it('should find link with path, query, fragment', () => {
+    expect(
+      Spanned.parse(`foo example.co.uk/foo/bar.html?uh=oh&_=123#anchor1 bar`, {
         maxLength: 280,
         countUrlsAs: 23,
       })
